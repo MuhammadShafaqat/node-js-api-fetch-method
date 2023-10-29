3a829faec3c1c8d1bf079c1e81d775cf0e1c5eef
Muhammad Shafaqat <muhammadshafaqat0344@gmail.com>
2023-10-30 00:37:20 +0500

    updated

--- frontend/signin.js
+++ frontend/signin.js
@@ -1,21 +1,66 @@
+const formData = document.getElementById('signinForm');
+formData.addEventListener('submit', async (event) => {
+  event.preventDefault();
 
-const myForm = document.getElementById('signinForm');
-myForm.addEventListener('submit', async (event) => {
-event.preventDefault();
+  const payload = new FormData(formData);
 
-const payload1 = new FormData(myForm);
-const formObjectData = {};
-          
-payload1.forEach((value, key) => {
-     formObjectData[key] = value;
-          });
-await fetch('http://localhost:3000/auth/signIn', {
+  const formDataObject = {};
+
+  payload.forEach((value, key) => {
+    formDataObject[key] = value;
+  });
+
+  fetch('http://localhost:3000/auth/signIn', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(formObjectData)     
-  }).then(response => response.json())
-  .then(data => console.log(data))
-  .catch(err => console.log(err))
-})
+    },
+    body: JSON.stringify(formDataObject)
+  })
+    .then(response => {
+      if (response.ok) {
+        alert('Authentication successful');
+        return response.json();
+      } else {
+        return response.json().then(data => {
+          console.log(data);
+          throw new Error('Authentication failed');
+        });
+      }
+    })
+    .then(data => {
+      // Handle successful response data (if needed)
+      console.log(data);
+      // Redirect to the login page
+      window.location.href = '/frontend/home.html'; // Replace with your login page URL
+    })
+    .catch(err => {
+      console.error(err);
+      alert('An error occurred while signig. Please try again.');
+    });
+});
+
+
+
+
+
+// const myForm = document.getElementById('signinForm');
+// myForm.addEventListener('submit', async (event) => {
+// event.preventDefault();
+
+// const payload1 = new FormData(myForm);
+// const formObjectData = {};
+          
+// payload1.forEach((value, key) => {
+//      formObjectData[key] = value;
+//           });
+// await fetch('http://localhost:3000/auth/signIn', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify(formObjectData)     
+//   }).then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err))
+// })
